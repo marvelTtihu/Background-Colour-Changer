5671ec351a95efbee6e2e53bba35ace88fc54324
Marvel Tihu <abangpung01@gmail.com>
2026-04-08 10:58:01 +0800

    Added color name

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const btnPrimary = document.getElementById('btnprimary');
-const titleName = document.getElementById('titlename');
+const colorName = document.getElementById('colorname')
 
 function getRandomColor () {
     return '#' + Math.floor(Math.random() * 16777216).toString(16). padStart(6, '0');
 }
-function getRandomColor2 () {
-    return '#' + Math.floor(Math.random() * 16777216).toString(16). padStart(6, '0');
-}
-
 btnPrimary.addEventListener('click', function () {
     const randomColor = getRandomColor();
-    const randomColor2 = getRandomColor2();
     document.body.style.backgroundColor = randomColor;
-    titleName.style.color = randomColor2;
-    btnPrimary.style.backgroundColor = randomColor2;
+    colorName.textContent = "Color: " + randomColor;
 });
 
 
